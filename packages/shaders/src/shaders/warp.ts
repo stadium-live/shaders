2368407785f8ffe36dad8c84753dd2843d069240
Ksenia Kondrashova <kondrashova.ksenya@gmail.com>
2025-06-20 11:54:25 +0200

    warp speed adjustments (back to january values)

--- packages/shaders/src/shaders/warp.ts
+++ packages/shaders/src/shaders/warp.ts
@@ -55,7 +55,7 @@ void main() {
   vec2 uv = v_patternUV;
   uv *= .005;
 
-  float t = .01 * u_time;
+  float t = 0.0625 * u_time;
 
   float n1 = valueNoise(uv * 1. + t);
   float n2 = valueNoise(uv * 2. - t);
